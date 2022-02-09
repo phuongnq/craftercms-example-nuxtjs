83038fa0218030334fba3c0ba8243fdb5819d4d9
Phil Nguyen <nquangphuong@gmail.com>
2022-02-09 14:32:12 +0700

    Add ICE to about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -2,18 +2,21 @@
   <v-row>
     <v-col class="text-center">
       <img src="/craftercms.png" alt="CrafterCMS" class="mb-5" />
-      <div
-        v-bind:data-craftercms-model-path="model.path"
-        v-bind:data-craftercms-model-id="model.id"
-        v-bind:data-craftercms-field-id="model.title_s"
+      <h1
+        v-bind:data-craftercms-model-id="this.iceAttributes['data-craftercms-model-id']"
+        v-bind:data-craftercms-model-path="this.iceAttributes['data-craftercms-model-path']"
+        data-craftercms-field-id="title_s" data-craftercms-index="0"
       >
-        <h1>{{model.title_s}}</h1>
-      </div>
+        {{model.title_s}}
+      </h1>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { from } from 'rxjs';
+import { fetchIsAuthoring } from '@craftercms/ice';
+import { getICEAttributes, initInContextEditing }  from '@craftercms/experience-builder';
 import { getModel } from '../lib/api';
 
 export default {
@@ -21,10 +24,19 @@ export default {
   data() {
     return {
       model: {},
+      iceAttributes: {},
     }
   },
   async fetch() {
     this.model = await getModel("/site/website/about/index.xml");
-  }
+    const isAuthoring = from(fetchIsAuthoring());
+    this.iceAttributes = getICEAttributes({ model: this.model, isAuthoring });
+  },
+  mounted() {
+    initInContextEditing({
+      path: this.iceAttributes['data-craftercms-model-path'],
+      modelId: this.iceAttributes['data-craftercms-model-id'],
+    })
+  },
 }
 </script>
